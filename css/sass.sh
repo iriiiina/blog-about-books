baa2ee0ca92b02deeaec5bbd751335b59a020ec0
iriiiina <irina.ivanova@protonmail.com>
2017-12-05 14:24:04 +0200

    Improve CSS performance; fix typos

--- css/sass.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sass common.sass:common.css --style compressed --sourcemap=none
-sass blog.sass:blog.css --style compressed --sourcemap=none
-sass post.sass:post.css --style compressed --sourcemap=none
